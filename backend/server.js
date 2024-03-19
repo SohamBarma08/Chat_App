de5936683ab9b0e08fa63f829937039bf5d14669
Soham Barma <sohambarma483@gmail.com>
2024-03-20 00:41:56 +0530

    message and conversation routes  added

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
-import { connect } from "mongoose";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express()
 const PORT =5000 //process.env.PORT || 5000;
 
-dotenv.config();
+dotenv.config();    
 
 // app.get("/", (req,res) => {
 //     res.send("Hello World!!!");
 // });
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
 
 app.listen(5000,()=> {
     connectToMongoDB();
