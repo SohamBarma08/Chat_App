3026ee2950df342f85def75e86dce1cc7a137be1
Soham Barma <sohambarma483@gmail.com>
2024-03-29 11:17:41 +0530

    get conversation added

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,9 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import { app, server } from "./socket/socket.js";
+//import { app, server } from "./socket/socket.js";
 
-// const app = express()
+const app = express()
 const PORT = 5000 //process.env.PORT || 5000;
 
 dotenv.config();    
@@ -25,7 +25,7 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
-server.listen(5000,()=> {
+app.listen(5000,()=> {
     connectToMongoDB();
     console.log(`Server running on port ${PORT}`);
 });
